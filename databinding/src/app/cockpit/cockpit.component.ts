b5cd4993f5c86ae19c90339b64f32979a3769a2d
Hamza <hamzadeveloppementweb@gmail.com>
2020-05-26 17:04:45 +0200

    Adding alias for communication between child and parent

--- databinding/src/app/cockpit/cockpit.component.ts
+++ databinding/src/app/cockpit/cockpit.component.ts
@@ -6,7 +6,7 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
-  @Output() blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
   @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
   newServerName = '';
   newServerContent = '';
