ba1ad0850c8c711e9d02736b51ccb1fb78601b86
Hamza <hamzadeveloppementweb@gmail.com>
2020-05-27 22:34:00 +0200

    Adding rooting by @Output() & passing recipes from recipe-list to recipe-item by @Input()

--- courseProject/src/app/app.component.ts
+++ courseProject/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'courseProject';
+  redirection: string = 'recipes';
+
+  onNavigate(location) {
+    this.redirection = location;
+  }
 }
