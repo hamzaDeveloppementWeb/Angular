53d89bc8c0525b606227a2f08f862390e2fc716d
Hamza <hamzadeveloppementweb@gmail.com>
2020-05-29 15:35:14 +0200

    Creating a basic directive and adding it

--- directives/src/app/app.module.ts
+++ directives/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BasicHighlightDirective} from "./basic-highlight/basic-highlight.directive";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
